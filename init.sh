d9709bf7b458417930ed9dc3a90781f08de4d957
phenix <sergio.lv2005@gmail.com>
2026-09-25 20:31:17 +0200

    script de inicio

--- init.sh
+++ init.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-docker build -t bot-hackroms-go .                  
+set -e 
+
+# ver si estas en el grupo docker
+if docker ps &> /dev/null
+then
+	CMD="docker"
+
+elif sudo -n docker ps &> /dev/null
+then
+	CMD="sudo docker"
+else
+	echo "Falta instalar docker, porfavor instalelo en su sistema operativo."
+	exit 1
+fi
+
+# construir imagen
+docker build -t bot-hackroms-go . &> /dev/null
+
+# ejecutar imagen
 docker run --rm -v $(pwd):/data bot-hackroms-go
