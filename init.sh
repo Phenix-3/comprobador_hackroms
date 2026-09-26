0a02b90fe4eb36ba39e9b08981b3d8b384b8c5a7
phenix <sergio.lv2005@gmail.com>
2026-09-26 17:17:00 +0200

    comprobacion sudo

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-set -e 
+set -e # salir en caso de que algo salga mal (primera vez que lo uso)
 
 # ver si estas en el grupo docker
 if docker ps &> /dev/null
 then
 	CMD="docker"
 
-elif sudo -n docker ps &> /dev/null
+elif sudo -n docker ps &> /dev/null # si no lo estas compruebo que puedas ejecutarlo como sudo
 then
 	CMD="sudo docker"
 else
-	echo "Falta instalar docker, porfavor instalelo en su sistema operativo."
+	echo "Falta instalar docker, porfavor instalelo en su sistema operativo o no tiene usted privilegios suficientes para utilizarlo."
 	exit 1
 fi
 
